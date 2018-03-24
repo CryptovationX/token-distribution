b0f5949d0c1dc7a3398b726e8fb14f74066ebfa4
itoonx <pah-bravo@windowslive.com>
2018-03-25 05:53:07 +0700

    Test: Added check the token correct constants information

--- test/constants.js
+++ test/constants.js
@@ -1,11 +1,22 @@
 
 // Address of the owner of creator
-const creatorAddr = "0xF1Aa8e2a9A6777E232c36Fa9BEe249A333D7E96d";
+const creatorAddr = "0xa42b712e7e332582031ddce93dc3106f19636ecb";
+// Address of the receiver
+const receiverAddr = "0x9077a19de4ae8fcd0a80759a823d238c8dc5b5d6";
+
+// Vanity information of the token
+const tokenName = "CryptovationX";
+const tokenDecimals = 18;
+const tokenSymbol = "CXO";
 
 // Number of tokens minted to creator
 const maxTokenSupply = 900000000 * Math.pow(10, 18);
 
 module.exports = {
   creatorAddr: creatorAddr,
+  receiverAddr: receiverAddr,
+  tokenName: tokenName,
+  tokenDecimals: tokenDecimals,
+  tokenSymbol: tokenSymbol,
   maxTokenSupply: maxTokenSupply
 };
--- test/test.js
+++ test/test.js
@@ -1,15 +1,36 @@
+
+// var Web3 = require('web3');
+
 constants = require('./constants.js');
+secrets = require('../secrets.json');
 
 var CryptovationX = artifacts.require("./CryptovationX.sol");
 
 contract('CryptovationX', (accounts) => {
 
-    it("Should verify max total supply", function(done) {
-        CryptovationX.deployed().then(function(instance) {
-            return instance.INITIAL_SUPPLY.call();
-        }).then(function(supply) {
-            assert.equal(supply.toNumber(), constants.maxTokenSupply, "Wrong max total supply");
-        }).then(done);
+    var INSTANCE_DEPLOYED
+    var ABI_INTERFACE
+
+    describe("Check the token correct constants information", () => {
+
+        it("Should verify max total supply", async () => {
+            let instance = await CryptovationX.deployed();
+            let maxSupply = await instance.INITIAL_SUPPLY.call();
+            INSTANCE_DEPLOYED = instance;
+            ABI_INTERFACE = instance.abi;
+            assert.equal(maxSupply, constants.maxTokenSupply, "Wrong max total supply");
+        });
+
+        it('Should test correct setting of vanity information', async () => {
+            let name = await INSTANCE_DEPLOYED.name.call();
+            assert.strictEqual(name, constants.tokenName);
+
+            let decimals = await INSTANCE_DEPLOYED.decimals.call();
+            assert.strictEqual(decimals.toNumber(), constants.tokenDecimals);
+
+            let symbol = await INSTANCE_DEPLOYED.symbol.call();
+            assert.strictEqual(symbol, constants.tokenSymbol);
+        });
     });
 
 });
